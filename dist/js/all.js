18eed3f8e6e24c97c8fe65b24a8cc801415d5e90
Kundius Ruslan <kundius.ruslan@gmail.com>
2020-08-06 03:31:09 +0300

    qq

--- dist/js/all.js
+++ dist/js/all.js
@@ -20781,7 +20781,7 @@ var galleryThumbs = new Swiper('.js-detail-gallery-thumbs', {
   slidesPerView: 'auto',
   loop: true,
   freeMode: true,
-  centeredSlides: true,
+  // centeredSlides: true,
   loopedSlides: 5,
   //looped slides should be the same
   watchSlidesVisibility: true,
