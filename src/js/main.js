3e0429e5dec2b4cda5587ee993a0799a19a53f5f
Kundius Ruslan <kundius.ruslan@gmail.com>
2020-12-29 22:25:46 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -1427,18 +1427,18 @@ $('.js-home-slider').each(function () {
     // Product Gallery
     var galleryThumbs = new Swiper($thumbs[0], {
         direction: 'vertical',
-        // allowTouchMove: false,
-        // slidesPerView: 3,
-        // freeMode: true,
-        // loopedSlides: 6,
-        // watchSlidesVisibility: true,
-        // watchSlidesProgress: true,
-        // centerInsufficientSlides: true
+        allowTouchMove: false,
+        slidesPerView: 3,
+        freeMode: true,
+        loopedSlides: 6,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        centerInsufficientSlides: true
     });
     var galleryTop = new Swiper($main[0], {
         slidesPerView: 1,
         loop: true,
-        // loopedSlides: 6, //looped slides should be the same
+        loopedSlides: 6, //looped slides should be the same
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
