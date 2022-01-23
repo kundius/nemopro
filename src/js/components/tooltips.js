035c1dffe8772056d0f313a846335d86209d6da7
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 16:06:16 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -1,3 +1,23 @@
 const { computePosition } = require('@floating-ui/dom')
 
-console.log(computePosition)
+const referenceElements = document.querySelectorAll('[data-tooltip]')
+
+if (referenceElements.length > 0) {
+  referenceElements.forEach(referenceElement => {
+    const floatingElement = document.querySelector(referenceElement.dataset.tooltip)
+
+    function applyStyles({x = 0, y = 0, strategy = 'absolute'}) {
+      Object.assign(floatingElement.style, {
+        position: strategy,
+        left: `${x}px`,
+        top: `${y}px`
+      })
+    }
+
+    applyStyles()
+    
+    computePosition(referenceElement, floatingElement, {
+      placement: 'right'
+    }).then(applyStyles)
+  })
+}
