e84728a50b948f80923a1814d31df44ae70f9f73
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 15:58:55 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ function js() {
 }
 
 function browserifyJs() {
-  return src(config.srcDir + 'js/browserify/main.js')
+  return src(config.srcDir + 'js/browserify.js')
     .pipe(tap(function (file) {
       log.info('bundling ' + file.path);
       // replace file contents with browserify's bundle stream
