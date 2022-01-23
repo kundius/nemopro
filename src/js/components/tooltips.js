a705e05d66019f6d5103631c102779a8df1a447c
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 16:56:07 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -1,45 +1,9 @@
-// const { computePosition } = require('@floating-ui/dom')
-
-// const withApplyStyles = (el) => {
-//   return (params) => {
-//     Object.assign(el.style, {
-//       position: params.strategy,
-//       left: `${params.x}px`,
-//       top: `${params.y}px`
-//     })
-//   }
-// }
-
-// const referenceElements = document.querySelectorAll('[data-tooltip]')
-
-// if (referenceElements.length > 0) {
-//   referenceElements.forEach(referenceElement => {
-//     const floatingElement = document.querySelector(referenceElement.dataset.tooltip)
-//     const applyStyles = withApplyStyles(floatingElement)
-
-//     // function applyStyles({x = 0, y = 0, strategy = 'absolute'}) {
-//     //   Object.assign(floatingElement.style, {
-//     //     position: strategy,
-//     //     left: `${x}px`,
-//     //     top: `${y}px`
-//     //   })
-//     // }
-
-//     applyStyles({x: 0, y: 0, strategy: 'absolute'})
-    
-//     computePosition(referenceElement, floatingElement, {
-//       placement: 'right'
-//     }).then(applyStyles)
-//   })
-// }
 const tippy = require('tippy.js')
-// require('tippy.js/dist/tippy.css')
 
-// console.log(tippy)
-
-tippy.default('[data-tooltip-coupon]', {
+const couponInstance = tippy.default('[data-tooltip-coupon]', {
   allowHTML: true,
   theme: 'light',
   content: '<div style="text-align: center">На 100% опубликованной суммы<br>Вы можете выбрать товары в подарок!<br><br>Подробнее в разделе «скидки и акции».</div>'
 })
 
+console.log(couponInstance)
