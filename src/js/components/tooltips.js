44c6295c6ef8d2a82678e42171a6e3ae7d8786e2
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 22:12:48 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -3,7 +3,7 @@ const tippy = require('tippy.js')
 const couponInstance = tippy.default('[data-tooltip-coupon]', {
   allowHTML: true,
   theme: 'light',
-  trigger: 'mouseenter click',
+  trigger: 'mouseenter touchstart',
   hideOnClick: 'toggle',
   content: '<div style="text-align: center">На 100% опубликованной суммы<br>Вы можете выбрать товары в подарок!<br><br>Подробнее в разделе «скидки и акции».</div>'
 })
