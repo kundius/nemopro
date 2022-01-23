1794ca0151b1c6984895c34e8f186993932a4dc2
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 16:41:16 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -39,6 +39,7 @@ console.log(tippy)
 
 tippy.default('[data-tooltip-coupon]', {
   allowHTML: true,
+  theme: 'light',
   content: 'На 100% опубликованной суммы<br>Вы можете выбрать товары в подарок!<br><br>Подробнее в разделе «скидки и акции».'
 })
 
