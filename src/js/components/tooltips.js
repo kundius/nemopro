a6e7c7bcae486dee8cbe002cc9272d48e1f46314
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 21:59:54 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -3,6 +3,7 @@ const tippy = require('tippy.js')
 const couponInstance = tippy.default('[data-tooltip-coupon]', {
   allowHTML: true,
   theme: 'light',
+  trigger: 'mouseenter click',
   hideOnClick: 'toggle',
   content: '<div style="text-align: center">На 100% опубликованной суммы<br>Вы можете выбрать товары в подарок!<br><br>Подробнее в разделе «скидки и акции».</div>'
 })
@@ -16,19 +17,19 @@ window.addEventListener('scroll', () => {
   // window.dispatchEvent(new Event('touchmove'))
   // window.dispatchEvent(new Event('touchcancel'))
   // window.dispatchEvent(new Event('touchend'))
-  couponInstance.forEach(instance => {
-  //   // instance.hideWithInteractivity(e)
-    // instance.hide()
-    // instance.show()
-    // instance.disable()
-    instance.setProps({
-      trigger: 'click'
-    })
-    // instance.setProps({
-    //   // touch: true
-    //   triggerTarget: null
-    // })
-    // tippy.hideAll()
-    // instance.reference.dispatchEvent(new Event('touchend'))
-  })
+  // couponInstance.forEach(instance => {
+  // //   // instance.hideWithInteractivity(e)
+  //   // instance.hide()
+  //   // instance.show()
+  //   // instance.disable()
+  //   // instance.setProps({
+  //   //   trigger: 'click'
+  //   // })
+  //   // instance.setProps({
+  //   //   // touch: true
+  //   //   triggerTarget: null
+  //   // })
+  //   // tippy.hideAll()
+  //   // instance.reference.dispatchEvent(new Event('touchend'))
+  // })
 })
