e1524fa8be9ae07cbc5c50bed87d99cf39acee0d
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-23 21:45:44 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -10,13 +10,19 @@ const couponInstance = tippy.default('[data-tooltip-coupon]', {
 console.log(couponInstance)
 
 window.addEventListener('scroll', (e) => {
-  // tippy.hideAll()
-  couponInstance.forEach(instance => {
+  tippy.hideAll()
+  window.dispatchEvent(new Event('click'))
+  window.dispatchEvent(new Event('touchstart'))
+  window.dispatchEvent(new Event('touchmove'))
+  window.dispatchEvent(new Event('touchcancel'))
+  window.dispatchEvent(new Event('touchend'))
+  // couponInstance.forEach(instance => {
   //   // instance.hideWithInteractivity(e)
-  //   // instance.hide()
+    // instance.hide()
+    // instance.show()
   //   // instance.disable()
   //   // instance.enable()
   //   tippy.hideAll()
-    instance.reference.dispatchEvent(new Event('touchend'))
-  })
+    // instance.reference.dispatchEvent(new Event('touchend'))
+  // })
 })
