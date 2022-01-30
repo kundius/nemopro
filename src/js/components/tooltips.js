cc7edde0af071fff44d0ac206c5c7a9363579ad5
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-01-30 21:33:37 +0300

    upd

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -1,13 +1,19 @@
 const tippy = require("tippy.js");
 
 function initCoupon() {
-  tippy.default("[data-tooltip-coupon]", {
-    allowHTML: true,
-    theme: "light",
-    trigger: "mouseenter click",
-    content:
-      '<div style="text-align: center">На 100% опубликованной суммы<br>Вы можете выбрать товары в подарок!<br><br>Подробнее в разделе «скидки и акции».</div>',
-  });
+  const all = document.querySelectorAll("[data-tooltip-coupon]");
+  if (all.length > 0) {
+    all.forEach((el) => {
+      if (el.couponTippyInit) return;
+      tippy.default(el, {
+        allowHTML: true,
+        theme: "light",
+        trigger: "mouseenter click",
+        content:
+          '<div style="text-align: center">На 100% опубликованной суммы<br>Вы можете выбрать товары в подарок!<br><br>Подробнее в разделе «скидки и акции».</div>',
+      });
+    });
+  }
 }
 
 initCoupon();
@@ -15,13 +21,6 @@ initCoupon();
 $(document).on("mse2_load", initCoupon);
 
 $(document).on("pdopage_load", initCoupon);
-// $(document).on("mse2_load", function (a, b, c) {
-//   console.log('mse2_load', a, b, c)
-// });
-
-// $(document).on("pdopage_load", function (a, b, c) {
-//   console.log('pdopage_load', a, b, c)
-// });
 
 const tooltipPriceTemplate = document.getElementById("tooltip-price");
 if (tooltipPriceTemplate) {
