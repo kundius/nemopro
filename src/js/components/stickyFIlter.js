5716beb60c6efabd976435d5840d4ccdfeb468c6
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-02-10 15:59:55 +0300

    upd

--- src/js/components/stickyFIlter.js
+++ src/js/components/stickyFIlter.js
@@ -1,23 +1,35 @@
-function initFilterBar () {
-  // if ($.cookie('show-filters') == 'true') {
-  //     $('.filters-wrap').removeClass('filters-wrap_hidden');
-  //     $('.filters-toggle-on-moblie').addClass('filters-toggle-on-moblie_active');
-  //     window.dispatchEvent(new Event('resize'));
-  // }
+const Sticksy = require('sticksy')
 
-  $(document).on('click', '.filters-wrap__toggle', function() {
-      $('.filters-wrap').toggleClass('filters-wrap_hidden');
-      $('.filters-toggle-on-moblie').toggleClass('filters-toggle-on-moblie_active');
-      // $.cookie('show-filters', !$('.filters-wrap').hasClass('filters-wrap_hidden'));
-      window.dispatchEvent(new Event('resize'));
-  });
+const stickyEl = document.querySelector('.filters-wrap')
 
-  $('.filters-toggle-on-moblie').on('click', function() {
-      $('.filters-toggle-on-moblie').toggleClass('filters-toggle-on-moblie_active');
-      $('.filters-wrap').toggleClass('filters-wrap_hidden');
-      // $.cookie('show-filters', !$('.filters-wrap').hasClass('filters-wrap_hidden'));
-      window.dispatchEvent(new Event('resize'));
-  });
+if (stickyEl) {
+  const sticky = new Sticksy(stickyEl, {
+    topSpacing: 60
+  })
+
 }
-initFilterBar();
+
+
+// function initFilterBar () {
+//   // if ($.cookie('show-filters') == 'true') {
+//   //     $('.filters-wrap').removeClass('filters-wrap_hidden');
+//   //     $('.filters-toggle-on-moblie').addClass('filters-toggle-on-moblie_active');
+//   //     window.dispatchEvent(new Event('resize'));
+//   // }
+
+//   $(document).on('click', '.filters-wrap__toggle', function() {
+//       $('.filters-wrap').toggleClass('filters-wrap_hidden');
+//       $('.filters-toggle-on-moblie').toggleClass('filters-toggle-on-moblie_active');
+//       // $.cookie('show-filters', !$('.filters-wrap').hasClass('filters-wrap_hidden'));
+//       window.dispatchEvent(new Event('resize'));
+//   });
+
+//   $('.filters-toggle-on-moblie').on('click', function() {
+//       $('.filters-toggle-on-moblie').toggleClass('filters-toggle-on-moblie_active');
+//       $('.filters-wrap').toggleClass('filters-wrap_hidden');
+//       // $.cookie('show-filters', !$('.filters-wrap').hasClass('filters-wrap_hidden'));
+//       window.dispatchEvent(new Event('resize'));
+//   });
+// }
+// initFilterBar();
 
