7fa4058feea3d1312ad6421ec1b96852383760f5
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-02-13 02:27:51 +0300

    upd

--- src/js/browserify.js
+++ src/js/browserify.js
@@ -1,3 +1,3 @@
-require('./components/tooltips.js')
-require('./components/stickyFIlter.js')
-require('./components/headerSearch.js')
+// require('./components/tooltips.js')
+// require('./components/stickyFIlter.js')
+// require('./components/headerSearch.js')
