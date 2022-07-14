1ab4fc78248ef9c2bdf60ecea9d2ba1ca8f89a7d
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-07-14 11:31:05 +0000

    upd

--- src/js/components/headerSearch.js
+++ src/js/components/headerSearch.js
@@ -38,8 +38,29 @@ if (toggle && search) {
 jQuery(function() {
   console.log('loaded', $('input[name="query"]'));
   $('input[name="query"]').autocomplete({
+    change: function( event, ui ) {
+      console.log('change');
+    },
+    close: function( event, ui ) {
+      console.log('close');
+    },
     create: function( event, ui ) {
-      console.log(ui);
+      console.log('create');
+    },
+    focus: function( event, ui ) {
+      console.log('focus');
+    },
+    open: function( event, ui ) {
+      console.log('open');
+    },
+    response: function( event, ui ) {
+      console.log('response');
+    },
+    search: function( event, ui ) {
+      console.log('search');
+    },
+    select: function( event, ui ) {
+      console.log('select');
     }
   });
 });
