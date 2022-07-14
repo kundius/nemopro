c0444ef89dcb7d1b9ff841c7416d22c7c26d2ab4
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-07-14 11:32:56 +0000

    upd

--- src/js/components/headerSearch.js
+++ src/js/components/headerSearch.js
@@ -38,30 +38,30 @@ if (toggle && search) {
 jQuery(function() {
   console.log('loaded', $('input[name="query"]'));
   $('input[name="query"]').autocomplete({
-    change: function( event, ui ) {
-      console.log('change');
-    },
-    close: function( event, ui ) {
-      console.log('close');
-    },
-    create: function( event, ui ) {
-      console.log('create');
-    },
-    focus: function( event, ui ) {
-      console.log('focus');
-    },
+    // change: function( event, ui ) {
+    //   console.log('change');
+    // },
+    // close: function( event, ui ) {
+    //   console.log('close');
+    // },
+    // create: function( event, ui ) {
+    //   console.log('create');
+    // },
+    // focus: function( event, ui ) {
+    //   console.log('focus');
+    // },
     open: function( event, ui ) {
-      console.log('open');
+      console.log('open', ui);
     },
-    response: function( event, ui ) {
-      console.log('response');
-    },
-    search: function( event, ui ) {
-      console.log('search');
-    },
-    select: function( event, ui ) {
-      console.log('select');
-    }
+    // response: function( event, ui ) {
+    //   console.log('response');
+    // },
+    // search: function( event, ui ) {
+    //   console.log('search');
+    // },
+    // select: function( event, ui ) {
+    //   console.log('select');
+    // }
   });
 });
 
