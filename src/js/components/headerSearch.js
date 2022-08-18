a292ac0fceba541ffcc4e178b872dd517db61ce4
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-08-18 15:48:16 +0000

    upd

--- src/js/components/headerSearch.js
+++ src/js/components/headerSearch.js
@@ -6,7 +6,7 @@ if (toggle && search) {
   const input = search.querySelector('[name="query"]');
   const $input = $(input);
   // let autocompleteInitialized = false;
-  let prevScroll = window.scrollY;
+  let prevScroll = null;
   let scrollDir = 1;
   let prevScrollDir = 1;
   let scrolled = 0;
@@ -39,6 +39,10 @@ if (toggle && search) {
   };
 
   const onScroll = (e) => {
+    if (prevScroll === null) {
+      prevScroll = window.scrollY;
+    }
+
     scrollDir = window.scrollY >= prevScroll ? 1 : -1;
 
     if (scrollDir != prevScrollDir) {
