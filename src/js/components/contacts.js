0214af6426f94d02d84560d7c18c1ba097793cfb
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-11-27 01:33:33 +0300

    upd

--- src/js/components/contacts.js
+++ src/js/components/contacts.js
@@ -1,34 +1,41 @@
-const handler = document.querySelector("#cdek-handler");
-const map = document.querySelector("#cdek-map");
+let init = false;
 
-if (handler && map) {
-  let init = false
+const widgets = document.querySelectorAll(".cdek-widget") || [];
+widgets.forEach((widget) => {
+  const placeholder = widget.querySelector(".cdek-placeholder");
+  const handler = widget.querySelector(".cdek-placeholder__button");
+  const target = widget.dataset.target;
+  const city = widget.dataset.city;
+
+  const load = () => {
+    new ISDEKWidjet({
+      defaultCity: city,
+      link: target,
+      path: "assets/nemopro/cdek/scripts/",
+      servicepath: "assets/nemopro/cdek/scripts/service.php",
+      country: "Россия",
+      choose: false,
+      hidedress: true,
+      hidecash: true,
+      hidedelt: true,
+    });
+    placeholder.classList.add('cdek-placeholder_hidden')
+  };
+
   handler.addEventListener("click", function (e) {
     e.preventDefault();
 
-    if (init) return
-
-    const script = document.createElement("script");
-    script.id = 'ISDEKscript';
-    script.onload = function () {
-      console.log(map.dataset.city)
-      
-      console.log(new ISDEKWidjet({
-        defaultCity: map.dataset.city,
-        link: "cdek-map",
-        path: "assets/nemopro/cdek/scripts/",
-        servicepath: "assets/nemopro/cdek/scripts/service.php",
-        country: 'Россия',
-        choose: false,
-        hidedress: true,
-        hidecash: true,
-        hidedelt: true,
-      }));
-
-      init = true;
-    };
-    script.src = "assets/nemopro/cdek/widjet.js";
-
-    document.head.appendChild(script);
+    if (init) {
+      load();
+    } else {
+      const script = document.createElement("script");
+      script.id = "ISDEKscript";
+      script.onload = function () {
+        load();
+        init = true;
+      };
+      script.src = "assets/nemopro/cdek/widjet.js";
+      document.head.appendChild(script);
+    }
   });
-}
+});
