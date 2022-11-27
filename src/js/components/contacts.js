280c1b57a50daf7bdf2b8c2d6645d479b3545afc
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-11-27 16:59:12 +0300

    upd

--- src/js/components/contacts.js
+++ src/js/components/contacts.js
@@ -56,6 +56,8 @@ ymapsWidgets.forEach((widget) => {
 
   let initMap = false;
 
+  placeholder.style.backgroundImage = `url("https://api-maps.yandex.ru/services/constructor/1.0/static/?um=${um}&amp;width=1158&amp;height=320&amp;lang=ru_RU")`
+
   handler.addEventListener("click", function (e) {
     e.preventDefault();
 
